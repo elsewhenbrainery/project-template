a22b375e32507fa242c69974650eeadec93d2d91
Andy Christianson <achristianson@asuslaptop>
2014-01-13 20:38:30 -0500

    added example function, variables, comments

--- project.js
+++ project.js
@@ -1,13 +1,26 @@
 var sketchProc = function(processing) {
 	
+    // put variables here, just like kahn academy
+    var exampleVar = 10;
+
+    // put functions here, just like kahn academy
     var exampleFunction = function() {
 	processing.rect(30, 20, 55, 55);
     };
 
     // this code is executed once when the program is started
     processing.setup = function() {
+
+	// set up the size of the canvas (you probably don't want to change this!)
 	processing.size(800, 600);
-	processing.text("Hello, World!", 10, 10);
+
+	// some drawing commands; feel free to change these
+	processing.fill(255, 0, 0);
+
+	// draw some text
+	processing.text("Hello, World!", exampleVar, exampleVar);
+
+	// call a function (defined up above)
 	exampleFunction();
     };
 
