dec067cc59a311c4b52c54f1a8bbb952c01807ae
elsewhenbrainery <cara@elsewhenbrainery.com>
2014-01-13 20:33:37 -0500

    added example function

--- project.js
+++ project.js
@@ -1,9 +1,14 @@
-function sketchProc(processing) {
+var sketchProc = function(processing) {
+	
+    var exampleFunction = function() {
+	processing.rect(30, 20, 55, 55);
+    }
 
     // this code is executed once when the program is started
     processing.setup = function() {
 	processing.size(800, 600);
 	processing.text("Hello, World!", 10, 10);
+	exampleFunction();
     };
 
     // override draw function, by default it will be called 60 times per second
